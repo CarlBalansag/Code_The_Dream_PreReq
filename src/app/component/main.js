59084c6771df1d7a52d3ca8457688c26b2914422
Carl <Carlgbt@gmail.com>
2025-06-24 21:09:28 -0700

    fixing mobile view

--- src/app/component/main.js
+++ src/app/component/main.js
@@ -1,93 +1,193 @@
-    "use client";
-    import { useState, useEffect } from "react";
-    import LiveSong, { fetchCurrentlyPlaying } from "./spotify component/live_song";
-    import RecentlyPlayedList from "./spotify component/recently_played_list";
-    import PremiumTopTracks from "./spotify component/premiumTopTracks";
-    import PremiumAlbum from "./spotify component/premiumAlbum";
-    import UserTopTracks from "./spotify component/user_top_tracks";
-    import UserTopArtists from "./spotify component/user_top_artists";
+"use client";
+import { useState, useEffect } from "react";
+import LiveSong, { fetchCurrentlyPlaying } from "./spotify component/live_song";
+import RecentlyPlayedList from "./spotify component/recently_played_list";
+import PremiumTopTracks from "./spotify component/premiumTopTracks";
+import PremiumAlbum from "./spotify component/premiumAlbum";
+import UserTopTracks from "./spotify component/user_top_tracks";
+import UserTopArtists from "./spotify component/user_top_artists";
 
-    export default function CurrentlyPlaying({ accessToken, premium, name }) {
-    const [song, setSong] = useState(null);                 //data of currently playing song
-    const [isPlaying, setIsPlaying] = useState(false);      //Checks if a song is currently playing
-    const [songID, setSongID] = useState(null);             //gets the songs ID
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import {
+Navigation,
+Pagination,
+Mousewheel,
+Keyboard,
+} from "swiper/modules";
 
-    const getSong = async () => {
-    //exits function if there is no accessToken
+export default function CurrentlyPlaying({ accessToken, premium, name }) {
+const [song, setSong] = useState(null);
+const [isPlaying, setIsPlaying] = useState(false);
+const [songID, setSongID] = useState(null);
+const [activeIndex, setActiveIndex] = useState(0);
+
+const swipeHints = [
+    "Swipe right for Top Tracks",
+    "Swipe right for Recently Played",
+    "Swipe left to go back to Artists",
+];
+
+const getSong = async () => {
     if (!accessToken) return;
 
-    //gets the current playing song from the function fetchCurrentlyPlaying
     const currentSong = await fetchCurrentlyPlaying(accessToken);
 
-    if (currentSong && currentSong.item) {    //if there is a song that is currently playing and there is song data
-    setIsPlaying(currentSong.is_playing);     //update playback status
-    const newSongId = currentSong.item.id;    // Get the ID of the currently playing song
+    if (currentSong && currentSong.item) {
+    setIsPlaying(currentSong.is_playing);
+    const newSongId = currentSong.item.id;
 
-    if (songID !== newSongId) {     //if the useState songID is different from the current song playing 
-        setSongID(newSongId);       // Update the store SongID
-        setSong(currentSong);       // Save the full song object to state
+    if (songID !== newSongId) {
+        setSongID(newSongId);
+        setSong(currentSong);
     }
-    } else {                        // No song is playing – clear the song state and set playing to false
+    } else {
     setSong(null);
     setIsPlaying(false);
     }
-    };
+};
 
-    useEffect(() => {
-    if (!accessToken) return;     //If there is no access token don't continue
-    getSong();                    //Gets the currently playing song 
-    const interval = setInterval(() => getSong(), 3000);      // Set up an interval to fetch the song every 3 seconds
-    return () => clearInterval(interval);     //Clean up the interval when component unmounts or accessToken changes
-    }, [accessToken]);
+useEffect(() => {
+    if (!accessToken) return;
+    getSong();
+    const interval = setInterval(() => getSong(), 3000);
+    return () => clearInterval(interval);
+}, [accessToken]);
 
-    return (
-    <div>
+return (
+    <div className="absolute inset-0 overflow-hidden">
     {premium ? (
-        song && song.item ? ( 
-        <div className="flex flex-col gap-8 md:flex-row"> {/* Div is only visible when the user is a premium member and also is currently playing a song */}
+        song && song.item ? (
+        <div className="flex flex-col gap-8 md:flex-row">
             <div className="basis-1/3">
-                <LiveSong song={song} isPlaying={isPlaying} accessToken={accessToken} getSong={getSong}/> {/* Component shows the currently playing song and also has playback control */}
+            <LiveSong
+                song={song}
+                isPlaying={isPlaying}
+                accessToken={accessToken}
+                getSong={getSong}
+            />
             </div>
             <div className="basis-1/3">
-                <PremiumTopTracks artistId={song.item.artists[0].id} accessToken={accessToken}/> {/* Gets the currently playing artist top music */}
+            <PremiumTopTracks
+                artistId={song.item.artists[0].id}
+                accessToken={accessToken}
+            />
             </div>
             <div className="basis-1/3">
-                <PremiumAlbum artistId={song.item.artists[0].id} accessToken={accessToken}/> {/* Gets the currently playing artist albums */}
+            <PremiumAlbum
+                artistId={song.item.artists[0].id}
+                accessToken={accessToken}
+            />
             </div>
         </div>
         ) : (
-        <div className="text-center"> {/* Div is only visible when the user is a premium member and is not currently playing a song */}
-            <div className="flex flex-row w-full">
-                <div className="text-white p-4 text-center basis-1/3">
-                    <UserTopArtists accessToken={accessToken} /> {/* Gets the users top listed artist */}
+        <div className="text-center w-full h-full">
+            {/* Mobile View */}
+            <div className="block lg:hidden flex justify-center px-4 relative overflow-visible w-full ">
+            <div className="w-full max-w-[600px] relative overflow-visible">
+                <div className="text-sm text-red-400 mb-2 text-center">
+                {swipeHints[activeIndex]}
                 </div>
-            <div className="basis-1/3">
-                <UserTopTracks accessToken={accessToken} /> {/* Gets the users top listed music */}
+                <Swiper
+                cssMode={true}
+                navigation={true}
+                pagination={true}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="mySwiper swiper-custom"
+                onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+                >
+                <SwiperSlide>
+                    <div className="overflow-hidden p-4">
+                    <UserTopArtists accessToken={accessToken} />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="overflow-hidden p-4">
+                    <UserTopTracks accessToken={accessToken} />
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div className="overflow-hidden p-4">
+                    <RecentlyPlayedList accessToken={accessToken} name={name} />
+                    </div>
+                </SwiperSlide>
+                </Swiper>
             </div>
-            <div className="basis-1/3">
-                <RecentlyPlayedList accessToken={accessToken} name={name} /> {/* Gets the users recently played songs */}
+            </div>
+
+            {/* Desktop View */}
+            <div className="hidden lg:flex flex-row w-full">
+            <div className="text-white pl-7 text-center basis-1/3">
+                <UserTopArtists accessToken={accessToken} />
+            </div>
+            <div className="pl-7 basis-1/3">
+                <UserTopTracks accessToken={accessToken} />
+            </div>
+            <div className="pl-7 basis-1/3">
+                <RecentlyPlayedList accessToken={accessToken} name={name} />
             </div>
             </div>
         </div>
         )
     ) : (
-        <div className="text-center"> {/* Div is visible when the user is not a premium member */}
+        <div className="text-center w-full h-full">
         <p className="text-red-500 text-xl font-semibold mb-4">
             Not a Premium Member
         </p>
-        <div className="flex flex-row w-full">
-            <div className="text-white p-4 text-center basis-1/3">
-                <UserTopArtists accessToken={accessToken} />
+
+        {/* Mobile View */}
+        <div className="block lg:hidden flex justify-center px-4 overflow-hidden w-full">
+            <div className="w-full max-w-md">
+            <div className="text-sm text-red-400 mb-2 text-center">
+                {swipeHints[activeIndex]}
             </div>
-            <div className="basis-1/3">
-                <UserTopTracks accessToken={accessToken} />
+            <Swiper
+                cssMode={true}
+                navigation={true}
+                pagination={true}
+                mousewheel={true}
+                keyboard={true}
+                modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                className="mySwiper swiper-custom"
+                onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
+            >
+                <SwiperSlide>
+                <div className="overflow-hidden p-4">
+                    <UserTopArtists accessToken={accessToken} />
+                </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className="overflow-hidden p-4">
+                    <UserTopTracks accessToken={accessToken} />
+                </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                <div className="overflow-hidden p-4">
+                    <RecentlyPlayedList accessToken={accessToken} name={name} />
+                </div>
+                </SwiperSlide>
+            </Swiper>
             </div>
-            <div className="basis-1/3">
-                <RecentlyPlayedList accessToken={accessToken} name={name} />
+        </div>
+
+        {/* Desktop View */}
+        <div className="hidden lg:flex flex-row w-full">
+            <div className="text-white pl-7 text-center basis-1/3">
+            <UserTopArtists accessToken={accessToken} />
+            </div>
+            <div className="pl-7 basis-1/3">
+            <UserTopTracks accessToken={accessToken} />
+            </div>
+            <div className="pl-7 basis-1/3">
+            <RecentlyPlayedList accessToken={accessToken} name={name} />
             </div>
         </div>
         </div>
     )}
     </div>
-    );
-    }
+);
+}
